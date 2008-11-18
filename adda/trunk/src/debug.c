d27407a808068ad2988f2dc698a0f3815ade3d22
Maxim <Maxim@45a4d0da-b139-11dd-ba98-ad202adc45ad>
2008-11-18 11:18:52 +0000

    Format of comments and indentation was changed; other minor textual changes. Code for calculating and saving different variants of Mueller matrix integrated over phi was shortened by implementing repeated blocks as inline function.

--- adda/trunk/src/debug.c
+++ adda/trunk/src/debug.c
@@ -16,38 +16,39 @@
 #include "const.h"
 
 #ifdef DEBUG
-/*============================================================*/
+//============================================================
 
 void DebugPrintf(const char *fname,const int line,const char *fmt, ... )
 {
-  va_list args;
-  char pr_line[MAX_PARAGRAPH];
-  extern int ringid;
+	va_list args;
+	char pr_line[MAX_PARAGRAPH];
+	extern int ringid;
 
-  va_start(args, fmt);
-  sprintf(pr_line,"DEBUG: %s:%d: ",fname,line);
-  vsprintf(pr_line+strlen(pr_line),fmt,args);
-  strcat(pr_line, "\n");
-  printf("(ringID=%i) %s",ringid,pr_line);
-  fflush(stdout);
-  va_end(args);
+	va_start(args, fmt);
+	sprintf(pr_line,"DEBUG: %s:%d: ",fname,line);
+	vsprintf(pr_line+strlen(pr_line),fmt,args);
+	strcat(pr_line, "\n");
+	printf("(ringID=%i) %s",ringid,pr_line);
+	fflush(stdout);
+	va_end(args);
 }
 
-/*=======================================================*/
+//=======================================================
 
 void FieldPrint (doublecomplex *x)
-  /* print current field at certain dipole -- not used; left for deep debug
-     NOT ROBUST, since DipoleCoord is not always available */
+/* print current field at certain dipole -- not used; left for deep debug; NOT ROBUST, since
+ * DipoleCoord is not always available
+ */
 {
-  extern FILE *logfile;
-  extern double *DipoleCoord;
-  int i=9810;
+	extern FILE *logfile;
+	extern double *DipoleCoord;
+	int i=9810;
 
-  i*=3;
-  fprintf(logfile,"Dipole coords = %.10E, %.10E, %.10E\n",
-          DipoleCoord[i],DipoleCoord[i+1],DipoleCoord[i+2]);
-  fprintf(logfile,"E = %.10E%+.10Ei,  %.10E%+.10Ei, %.10E%+.10Ei\n",
-          x[i][RE],x[i][IM],x[i+1][RE],x[i+1][IM],x[i+2][RE],x[i+2][IM]);
+	i*=3;
+	fprintf(logfile,"Dipole coordinates = %.10E, %.10E, %.10E\n",DipoleCoord[i],DipoleCoord[i+1],
+		DipoleCoord[i+2]);
+	fprintf(logfile,"E = %.10E%+.10Ei,  %.10E%+.10Ei, %.10E%+.10Ei\n",x[i][RE],x[i][IM],x[i+1][RE],
+		x[i+1][IM],x[i+2][RE],x[i+2][IM]);
 }
 
-#endif /* DEBUG */
+#endif // DEBUG
