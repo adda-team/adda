f4c688c97c93188c22866f58791041924e820de4
yurkin <yurkin@45a4d0da-b139-11dd-ba98-ad202adc45ad>
2008-11-24 19:41:20 +0000

    Moving directory tree one level higher.

--- src/io.h
+++ src/io.h
@@ -1,43 +1,39 @@
 /* FILE: io.h
  * AUTH: Maxim Yurkin
- * DESCR: i/o and error handling routines
+ * DESCR: io and error handling routines
  *
- * Copyright (C) 2006,2008 University of Amsterdam
+ * Copyright (C) 2006 M.A. Yurkin and A.G. Hoekstra
  * This code is covered by the GNU General Public License.
  */
 #ifndef __io_h
 #define __io_h
 
-#include <stdio.h>    // for file
-#include "function.h" // for function attributes
+#include <stdio.h>
 
-/* File locking is made quite robust, however it is a complex operation that can cause unexpected
- * behavior (permanent locks) especially when program is terminated externally (e.g. because of MPI
- * failure). Moreover, it is not ANSI C, hence may have problems on some particular systems.
- * Currently file locking functions are only in param.c
- */
+/* file locking is made quite robust, however it is a complex operation that
+   can cause unexpected behaviour (permanent locks) especially when
+   program is terminated externally (e.g. because of MPI failure).
+   Moreover, it is not ANSI C, hence may have problems on some particular systems.
+   Currently file locking functions are only in param.c */
 
-//#define NOT_USE_LOCK  // uncomment to disable file locking
-//#define ONLY_LOCKFILE // uncomment to use only lock file, without file locking over NFS
+/*#define NOT_USE_LOCK     /* uncomment to disable file locking */
 
 #ifndef NOT_USE_LOCK
-#	define USE_LOCK
-#	ifndef ONLY_LOCKFILE
-#		define LOCK_FOR_NFS // currently this works only for POSIX
-#	endif
+# define USE_LOCK
 #endif
 
-void WrapLines(char *str);
-char *WrapLinesCopy(const char *str);
-void LogError(int ErrCode,int who,const char *fname,int line,const char *fmt,...) ATT_PRINTF(5,6);
-void PrintError(const char *fmt, ... ) ATT_PRINTF(1,2) ATT_NORETURN;
+void LogError(int ErrCode,int who,const char *fname,int line,const char *fmt,...);
+void PrintError(const char *fmt, ... );
 void LogPending(void);
-void PrintBoth(FILE *file,const char *fmt, ... ) ATT_PRINTF(2,3);
+void PrintBoth(FILE *file,const char *fmt, ... );
 
-FILE *FOpenErr(const char *fname,const char *mode,int who,const char *err_fname,
-               int lineN) ATT_MALLOC;
+FILE *FOpenErr(const char *fname,const char *mode,int who,const char *err_fname,int lineN);
 void FCloseErr(FILE *file,const char *fname,int who,const char *err_fname,int lineN);
 void RemoveErr(const char *fname,int who,const char *err_fname,int lineN);
 void MkDirErr(const char *dirname,int who,const char *err_fname,int lineN);
 
-#endif // __io_h
+#endif /* __io_h */
+
+
+
+
