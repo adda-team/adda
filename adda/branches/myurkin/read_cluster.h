d14ac3a50b07d582c54dc06b47f1fade25ee2e7c
Maxim <Maxim@45a4d0da-b139-11dd-ba98-ad202adc45ad>
2008-11-24 09:00:18 +0000

    Removed an old branch.

--- adda/branches/myurkin/read_cluster.h
+++
@@ -1,8 +0,0 @@
-#define MIN(A,B) (((A) > (B)) ? (B) : (A))
-#define MAX(A,B) (((A) < (B)) ? (B) : (A))
-
-void format(char input[],char output[]);
-void read_cluster(char input[],int *ngrains,
-		  REAL *coords[3],REAL **radius,int **grain_material);
-void setup_box(int ngrains,REAL *coords[3],REAL *max);
-void free_aggregate(REAL *coords[3],REAL *radius,int *grain_material);
