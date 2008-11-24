175a1561bf5c22050a115b022a4185e48bf45dc2
yurkin <yurkin@45a4d0da-b139-11dd-ba98-ad202adc45ad>
2008-11-24 19:41:20 +0000

    Moving directory tree one level higher.

--- src/memory.h
+++ src/memory.h
@@ -4,22 +4,22 @@
  *        memory allocation and freeing
  *        also includes overflows checks
  *
- * Copyright (C) 2006-2008 University of Amsterdam
+ * Copyright (C) 2006-2007 University of Amsterdam
  * This code is covered by the GNU General Public License.
  */
 #ifndef __memory_h
 #define __memory_h
 
-#include <stddef.h>   // for size_t
-#include "function.h" // for function attributes
+#include <stddef.h>    /* for size_t */
+#include "function.h"  /* for function attributes */
 
 #define MBYTE 1048576.0
-// for conciseness
+/* for conciseness */
 #define OTHER_ARGUMENTS const int who,const char *fname,const int line,const char *name
 
 void CheckOverflow(double size,OTHER_ARGUMENTS);
 size_t MultOverflow(size_t a,size_t b,OTHER_ARGUMENTS);
-// allocate
+/* allocate */
 doublecomplex *complexVector(size_t size,OTHER_ARGUMENTS) ATT_MALLOC;
 double **doubleMatrix(size_t rows,size_t cols,OTHER_ARGUMENTS) ATT_MALLOC;
 double *doubleVector(size_t size,OTHER_ARGUMENTS) ATT_MALLOC;
@@ -30,20 +30,17 @@ unsigned short *ushortVector(size_t size,OTHER_ARGUMENTS) ATT_MALLOC;
 char *charVector(size_t size,OTHER_ARGUMENTS) ATT_MALLOC;
 unsigned char *ucharVector(size_t size,OTHER_ARGUMENTS) ATT_MALLOC;
 void *voidVector(size_t size,OTHER_ARGUMENTS) ATT_MALLOC;
-// reallocate
-double *doubleRealloc(double *ptr,const size_t size,OTHER_ARGUMENTS) ATT_MALLOC;
-// free
+/* free */
 void Free_cVector(doublecomplex *v);
 void Free_dMatrix(double **m,size_t rows);
 void Free_dVector2(double *v,size_t nl);
 void Free_iMatrix(int **m,size_t nrl,size_t nrh,size_t ncl);
 void Free_general(void *v);
 
-// macros to use for allocation and reallocation
+/* macros to use for allocation */
 #define MALLOC_VECTOR(vec,type,size,who) vec=type##Vector(size,who,POSIT,#vec)
 #define MALLOC_DVECTOR2(vec,nl,nh,who) vec=doubleVector2(nl,nh,who,POSIT,#vec)
 #define MALLOC_DMATRIX(vec,rows,cols,who) vec=doubleMatrix(rows,cols,who,POSIT,#vec)
 #define MALLOC_IMATRIX(vec,nrl,nrh,ncl,nch,who) vec=intMatrix(nrl,nrh,ncl,nch,who,POSIT,#vec)
-#define REALLOC_DVECTOR(vec,size,who) vec=doubleRealloc(vec,size,who,POSIT,#vec)
 
-#endif //__memory_h
+#endif /*__memory_h*/
