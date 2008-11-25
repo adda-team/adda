5b3026b4027657037c3d91032a22d733d141ebac
yurkin <yurkin@45a4d0da-b139-11dd-ba98-ad202adc45ad>
2008-11-25 06:52:28 +0000

    Copyright was updated to GPL 3.

--- trunk/src/parbas.h
+++ trunk/src/parbas.h
@@ -1,10 +1,21 @@
 /* File: parbas.h
- * $Author: yurkin $
- * $Date:: 2008-11-25 10:26:04 +0600 #$
+ * $Author$
+ * $Date::                            $
  * Descr: parallel basics; includes necessary headers and checks version of the standard.
  *
  * Copyright (C) 2007,2008 University of Amsterdam
- * This code is covered by the GNU General Public License.
+ * This file is part of ADDA.
+ *
+ * ADDA is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * ADDA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with ADDA. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 #ifndef __parbas_h
 #define __parbas_h
