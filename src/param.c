0504f529a3a92fc89530919a199a02e2a67220c4
yurkin <yurkin@45a4d0da-b139-11dd-ba98-ad202adc45ad>
2010-02-01 09:01:04 +0000

    "2010" was added to copyright statements of the updated files and of the whole code.

--- src/param.c
+++ src/param.c
@@ -5,7 +5,7 @@
  *        information; contains file locking routines
  *
  * Copyright (C) 2006-2008 University of Amsterdam
- * Copyright (C) 2009 Institute of Chemical Kinetics and Combustion & University of Amsterdam
+ * Copyright (C) 2009,2010 Institute of Chemical Kinetics and Combustion & University of Amsterdam
  * This file is part of ADDA.
  *
  * ADDA is free software: you can redistribute it and/or modify it under the terms of the GNU
@@ -1220,7 +1220,8 @@ PARSE_FUNC(test)
 PARSE_FUNC(V)
 {
 	char copyright[]="\n\nCopyright (C) 2006-2008 University of Amsterdam\n"
-		"Copyright (C) 2009 Institute of Chemical Kinetics and Combustion & University of Amsterdam\n"
+		"Copyright (C) 2009,2010 Institute of Chemical Kinetics and Combustion & University of "
+		"Amsterdam\n"
 		"This program is free software; you can redistribute it and/or modify it under the terms "
 		"of the GNU General Public License as published by the Free Software Foundation; either "
 		"version 3 of the License, or (at your option) any later version.\n\n"
