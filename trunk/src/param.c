2b6e81f65f0f762e7f2c587b6a218bfe3f09e718
yurkin <yurkin@45a4d0da-b139-11dd-ba98-ad202adc45ad>
2012-01-23 02:44:39 +0000

    Copyright year updated to 2012 for the whole package and in headers of a few files modified during previous commit.

--- trunk/src/param.c
+++ trunk/src/param.c
@@ -1342,7 +1342,7 @@ PARSE_FUNC(test)
 }
 PARSE_FUNC(V)
 {
-	char copyright[]="\n\nCopyright (C) 2006-2011 ADDA contributors\n"
+	char copyright[]="\n\nCopyright (C) 2006-2012 ADDA contributors\n"
 		"This program is free software; you can redistribute it and/or modify it under the terms "
 		"of the GNU General Public License as published by the Free Software Foundation; either "
 		"version 3 of the License, or (at your option) any later version.\n\n"
