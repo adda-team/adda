bd5e87e22fd951936bfaa235d0b175a3ba58b323
inzhevatkin <k.inzhevatkin@yandex.ru>
2022-01-17 15:15:23 +0700

    Update main.py
    Added command line options, including default options

--- examples/papers/2021_WKBr/WKBr/main.py
+++ examples/papers/2021_WKBr/WKBr/main.py
@@ -1,22 +1,45 @@
-from sys import argv
+import sys, getopt
 from WKBr import find_wkb_ef
 from rearrange_ef_file import rearrange_ef
 
 # The program prepares the WKBr field.
-# launch example: python main.py 10 1.1 16 simple_wkbr
+# launch example: python main.py -s 10 -m 1.1 -g 16 -t simple_wkbr
 
-if __name__ == "__main__":
-    size = float(argv[1])
-    m = float(argv[2])
-    grid = float(argv[3])
-    mytype = argv[4] # supported options: "simple_wkbr" (WKBrI), "complex_wkbr" (WKBrII), and "analytic" (conventional WKB)  
+def main(argv):
+    # default options:
+    size = 10
+    m = 1.1
+    grid = 16
+    type = "simple_wkbr" # supported options: "simple_wkbr" (WKBrI), "complex_wkbr" (WKBrII), and "analytic" (conventional WKB)
+    # read options from command line:
+    try:
+        opts, args = getopt.getopt(argv,"hs:m:g:t:")
+    except getopt.GetoptError:
+        print ('python main.py -s <size> -m <m> -g <grid> -t <type>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('python main.py -s <size> -m <m> -g <grid> -t <type>')
+            sys.exit()
+        elif opt == "-s":
+         size = float(arg)
+        elif opt == "-m":
+         m = float(arg)
+        elif opt == "-g":
+         grid = int(arg)
+        elif opt == "-t":
+         type = arg 
+         
     print("Current size = ", size)
     print("Current grid = ", grid)
     print("Current m = ", m)
-    print("Current WKBr type = ", mytype)
+    print("Current WKBr type = ", type)
     tail = str(size) + "-" + str(m) + "-" + str(grid) + ".dat"
-    find_wkb_ef(0, 0, 0, m, 0, size / 2, 1, mytype + "-" + tail, grid, type=mytype, find_grid=True)
+    find_wkb_ef(0, 0, 0, m, 0, size / 2, 1, type + "-" + tail, grid, type=type, find_grid=True)
 
     # change columns to get y-polarization:
     tail = str(size) + "-" + str(m) + "-" + str(grid)
-    rearrange_ef(mytype + "-" + tail + ".dat", mytype + "-" + tail + "-Y-component.dat")
+    rearrange_ef(type + "-" + tail + ".dat", type + "-" + tail + "-Y-component.dat")
+    
+if __name__ == '__main__':
+    main(sys.argv[1:])
