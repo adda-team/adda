1e5665fd073294481d352de60d28082763ab5211
Maxim Yurkin <yurkin@gmail.com>
2025-02-20 00:04:44 +0100

    updates version and copyright string

--- src/const.h
+++ src/const.h
@@ -16,7 +16,7 @@
 #define __const_h
 
 // version number (string)
-#define ADDA_VERSION "1.5.0-alpha2"
+#define ADDA_VERSION "1.5.0-alpha3"
 
 /* ADDA uses certain C99 extensions, which are widely supported by GNU and Intel compilers. However, they may be not
  * completely supported by e.g. Microsoft Visual Studio compiler. Therefore, we check the version of the standard here
